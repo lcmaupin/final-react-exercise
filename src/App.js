9a43ffdbd93bcbedec3e3fb90d5cf026290c353b
Luke Maupin <lukecmaupin@gmail.com>
2021-12-09 14:48:40 -0500

    Adds files with base and bonus components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+// 2a
+import MCUShows from "./mcu-shows/MCUShows";
+
+// BONUS 6a & 8a
+import { useState, useEffect } from 'react';
+
+// 5a
+const releaseDates = {
+  wandaVision: 'January 2021',
+  falconWinter: 'March 2021',
+  loki: 'June 2021',
+  whatIf: 'August 2021',
+  hawkeye: 'November 2021'
+};
+
+// BONUS 6b
+const mcuCharacters = [
+  'Scarlet Witch',
+  'Vision',
+  'Falcon',
+  'Winter Soldier',
+  'Loki',
+  'The Watcher',
+  'Hawkeye'
+];
 
 function App() {
+  // BONUS 6c
+  const [index, setIndex] = useState(0);
+
+  // BONUS 7a
+  const [num, setNum] = useState(0);
+
+  // BONUS 8a
+  // The function (1st argument) passed into the useEffect function/hook will run every time the state value ("num" in this example) that is passed into the array (2nd argument) is updated.
+  // NOTE: If nothing is passed in for a 2nd argument, then the function will run whenever any state values are updated.)
+  useEffect(() => {
+    alert("The Next MCU Character has been displayed");
+  }, [num]);
+
+  // BONUS 6f
+  function newIndex() {
+    const randomIndex = Math.floor(Math.random() * mcuCharacters.length);
+    setIndex(randomIndex);
+  }
+
+  // BONUS 7d
+  function newNum() {
+    if (num < mcuCharacters.length - 1) {
+      setNum(num + 1);
+    } else {
+      setNum(0);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* 2b */}
+      <h1>FINAL REACT EXERCISE</h1>
+      {/* 2c & 5b & BONUS 6f & 7d */}
+      <MCUShows
+        dates={releaseDates}
+        indexTwo={newIndex}
+        numTwo={newNum}
+      />
+      {/* BONUS 6d & 6f */}
+      <h1>Random MCU Character: {mcuCharacters[index]}</h1>
+      {/* BONUS 7b & 7d */}
+      <h1>Next MCU Character: {mcuCharacters[num]}</h1>
     </div>
   );
 }
---
+++ src/mcu-shows/MCUShows.js
@@ -0,0 +1,28 @@
+// 1a
+import './MCUShows.css';
+
+// 1b & 5c & BONUS 6f & 7d
+function MCUShows({dates, indexTwo, numTwo}){
+  // 1c
+  return(
+    // 4a
+    <div className="MCUShows">
+      {/* 3a */}
+      <h1 style={{fontFamily: 'Georgia', textDecoration: 'underline'}}>Disney+</h1>
+      <h2>MCU Show Release Dates</h2>
+      {/* 5c */}
+      <p>WandaVision: {dates.wandaVision}</p>
+      <p>The Falcon and the Winter Soldier: {dates.falconWinter}</p>
+      <p>Loki: {dates.loki}</p>
+      <p>What If...?: {dates.whatIf}</p>
+      <p>Hawkeye: {dates.hawkeye}</p>
+      {/* BONUS 6e & 6f */}
+      <button onClick={indexTwo}>RANDOM MCU CHARACTER</button>
+      {/* BONUS 7c & 7d */}
+      <button onClick={numTwo}>NEXT MCU CHARACTER</button>
+    </div>
+  );
+}
+
+// 1d
+export default MCUShows;
